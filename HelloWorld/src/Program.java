4e58b11dfcb5141e41ab5ede1d5a46313c3e964c
hasff <hugoferro@gmail.com>
2014-05-03 13:11:30 +0100

    second commit.

--- HelloWorld/src/Program.java
+++ HelloWorld/src/Program.java
@@ -3,7 +3,8 @@ public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("HelloWorld");
+		System.out.println("outra versao...");
 	}
 
 }
