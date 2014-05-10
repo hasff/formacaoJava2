a51418720070e1f904aa3e90ea5f049c10608f5e
hasff <hugoferro@gmail.com>
2014-05-10 09:49:28 +0100

    read args

--- HelloWorld/src/Program.java
+++ HelloWorld/src/Program.java
@@ -3,8 +3,31 @@ public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("HelloWorld");
-		System.out.println("outra versao...");
+		//System.out.println("HelloWorld");
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		
+		if (args != null && args.length > 0){
+			System.out.print("Lista de Parametros: ");
+			/*
+			for(String s : args){
+				System.out.print(s + " ");
+			}
+			*/
+			for (int i = 0; i < args.length; i++){
+				System.out.print(args[i]);
+				if (i != args.length-1){
+					System.out.print(" # ");
+				}
+			}
+		}
+		else{
+			System.out.println("(!) nao existem parametros!");
+		}
+		
+		System.out.println();
+		System.out.println();
+		System.out.println();
 	}
-
 }
