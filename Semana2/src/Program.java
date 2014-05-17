f898b6e0efac1ac1cb3be07ea6d5bd75a0372e08
hasff <hugoferro@gmail.com>
2014-05-17 12:55:50 +0100

    ...

--- Semana2/src/Program.java
+++ Semana2/src/Program.java
@@ -7,6 +7,25 @@ public class Program {
 		
 		int xpto = (int)(1/2.9);
 		System.out.println(xpto);
+		
+		int j = 50;			// 32 bits
+		char i = (char)j;	// 16 bits
+		
+		System.out.println(i);
+		
+		char a = 'U';
+		int b = a;
+		
+		System.out.println(b);
+		
+		boolean a1 = false;
+		boolean a2 = false;
+		boolean a3 = false;
+		boolean a4 = true;
+		
+		System.out.println();
+		
+		String $aa = "sk";
 	}
 
 }
