e2ed0a5b37d8ee1b274ffff9a83fb7f593360d81
hasff <hugoferro@gmail.com>
2014-05-31 12:46:09 +0100

    conditional operator + bitwise

--- Semana5/src/Program.java
+++ Semana5/src/Program.java
@@ -2,8 +2,11 @@
 public class Program {
 
 	public static void main(String[] args) {
-		Program.stringBuild();
-		Program.stringNormal();
+		//Program.stringBuild();
+		//Program.stringNormal();
+		
+		Program.conditionalOps();
+		Program.conditionalOpsBitwise();
 	}
 	
 	public static void stringBuild(){
@@ -28,5 +31,41 @@ public class Program {
 		System.out.println(a + " +++ " + b);
 	}
 
+	public static void conditionalOps(){
+		int i = 2; 
+		
+		if (i<1 && i++>3){
+			
+		}
+		
+		System.out.println(i);
+		
+		int j = 2; 
+		
+		if (j>1 || j++>3){
+			
+		}
+		
+		System.out.println(j);
+	}
+	
+	public static void conditionalOpsBitwise(){
+		int i = 2; 
+		
+		if (i<1 & i++>3){ // 0 & 0 => 0 
+			
+		}
+		
+		System.out.println(i);
+		
+		int j = 2; 
+		
+		if (j>1 | j++>3){
+			
+		}
+		
+		System.out.println(j);
+	}
+	
 }
 
